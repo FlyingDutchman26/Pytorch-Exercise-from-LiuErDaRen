3bc7eebfb845444d1700114aef754e7c173109c2
FlyingDutchman26 <2247778946@qq.com>
2022-10-15 23:07:55 +0800

    add ex_06.py

--- ex_06.py
+++ ex_06.py
@@ -1,23 +1,23 @@
 import torch
 
 x_data = torch.tensor([[1.0], [2.0], [3.0]])
-y_data = torch.tensor([[2.0], [4.0], [6.0]])
+y_data = torch.tensor([[0.0], [0.0], [1.0]])
 
 
-class LinearModel(torch.nn.Module):
+class LogisticRegressionModel(torch.nn.Module):
     def __init__(self) -> None:
-        super(LinearModel, self).__init__()
+        super(LogisticRegressionModel, self).__init__()
         # torch.nn.Linear 这也是一个类，同样继承于 torch.nn.Module，可以自动生成计算图
         self.Linear = torch.nn.Linear(1, 1)  # 实例化这个线性层 从 1维 到 1维 ，会生成对应参数（自动计算梯度）
 
     def forward(self, x):
-        y_pred = self.Linear(x)     # 继承于torch.nn.Module的类都具有__call__()方法
+        y_pred = torch.sigmoid(self.Linear(x))     # 继承于torch.nn.Module的类都具有__call__()方法
         return y_pred
 
 
-model = LinearModel()
+model = LogisticRegressionModel()
 
-criterion = torch.nn.MSELoss(reduction= 'sum')  # 意思是不取平均，这是新版写法， size_average已被取代
+criterion = torch.nn.BCELoss(reduction= 'sum')  # 意思是不取平均，这是新版写法， size_average已被取代
 # SGD优化器实现梯度下降， 梯度计算已经储存于model.parameters()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
